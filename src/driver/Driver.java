c1c8751af25d637720c80a23f0d6420f23a71181
Moritz <cmhoequist@gmail.com>
2016-11-16 12:24:45 -0600

    Initial commit

---
+++ src/driver/Driver.java
@@ -0,0 +1,28 @@
+package driver;
+
+/**
+ * Created by Moritz on 11/16/2016.
+ * <p></p>
+ */
+public class Driver {
+    public static void main(String[] args){
+        //TODO: write input handler
+        /*
+        First: user selects between graph & digraph
+        Then: user adds set of nodes and set of edges
+         */
+        //TODO: write graph constructor
+        /*
+        Adjacency list is constructed and printed
+        Adjacency matrix is constructed and printed
+        Graph diagram is constructed and displayed
+         */
+        //TODO: write graph algorithms
+        /*
+        BFS is executed. Print the nodes in order of visitation, one component per line.
+        DFS is executed. Print the nodes in order of visitation, one component per line.
+        Print a topological ordering for the graph (or error message if graph is not DAG).
+        Print list of SCCs for graph (or error message if graph is not digraph).
+         */
+    }
+}
