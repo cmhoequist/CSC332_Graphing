2bcae148790b9eebe312b0402e72ef5096c498f2
Moritz <cmhoequist@gmail.com>
2016-11-16 19:41:56 -0600

    Comments updated for clarity

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -7,18 +7,18 @@ import model.Graph;
  * Created by Moritz on 11/16/2016.
  * <p></p>
  */
-//TODO: input handler
+//TODO: input handler (class View)
         /*
         (?) First: user selects between graph & digraph
         (?) Then: user adds set of nodes and set of edges
          */
-//TODO: graph builder
+//TODO: graph builder (class Graph, might need additional Diagram class)
         /*
         (DONE) Adjacency list is constructed and printed
         (?) Adjacency matrix is constructed and printed
         (?) Graph diagram is constructed and displayed
          */
-//TODO: write graph model
+//TODO: write algorithms (class BFSVisitor, DFSVisitor)
         /*
         (Moritz) BFS is executed. Print the nodes in order of visitation, one component per line.
         (?) DFS is executed. Print the nodes in order of visitation, one component per line.
