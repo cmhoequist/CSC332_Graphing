0559b7e0e8a24910ef67844463526cf8f2f4404b
Moritz <cmhoequist@gmail.com>
2016-11-21 12:08:29 -0600

    Incremental view update

--- src/view/GraphPanel.java
+++ src/view/GraphPanel.java
@@ -1,5 +1,7 @@
 package view;
 
+import javax.swing.*;
+
 /**
  * Created by Moritz on 11/21/2016.
  * <p></p>
--- src/view/ProjectFrame.java
+++ src/view/ProjectFrame.java
@@ -1,10 +1,26 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Moritz on 11/21/2016.
  * <p></p>
  */
 public class ProjectFrame extends JFrame {
+    CardLayout cardLayout = new CardLayout();
+
+    public ProjectFrame(){
+        JPanel content = new JPanel(cardLayout);
+        getContentPane().setLayout(cardLayout);
+        add(new GraphPanel(), "graphpanel");
+        add(new AlgorithmPanel(), "algorithmpanel");
+
+        //Finish frame
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+
 }
