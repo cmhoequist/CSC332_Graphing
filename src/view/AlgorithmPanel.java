845bc4d66456e958af854cc9e8d3dff8abce38eb
Moritz <cmhoequist@gmail.com>
2016-11-21 12:00:18 -0600

    View package.

---
+++ src/view/AlgorithmPanel.java
@@ -0,0 +1,8 @@
+package view;
+
+/**
+ * Created by Moritz on 11/21/2016.
+ * <p></p>
+ */
+public class AlgorithmPanel extends JPanel {
+}
---
+++ src/view/ProjectFrame.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.*;
+
+/**
+ * Created by Moritz on 11/21/2016.
+ * <p></p>
+ */
+public class ProjectFrame extends JFrame {
+}
